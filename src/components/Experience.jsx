c7cf2610c3105573b2492d6ae136f41c43e0ccf6
Jivan Acharya <jivan9181@gmail.com>
2023-12-16 17:09:38 +0545

    add chair configurator

---
+++ src/components/Experience.jsx
@@ -0,0 +1,49 @@
+import {
+  Float,
+  MeshReflectorMaterial,
+  PresentationControls,
+  Stage,
+} from "@react-three/drei";
+import Chair from "./Chair";
+
+const Experience = () => {
+  return (
+    <>
+      <PresentationControls
+        speed={1.5}
+        global
+        polar={[-0.7, Math.PI / 4]}
+        rotation={[Math.PI / 8, Math.PI / 4, 0]}
+      >
+        <Stage environment="city" intensity={0.6} castShadow={false}>
+          <Float
+            speed={1} // Animation speed, defaults to 1
+            rotationIntensity={1} // XYZ rotation intensity, defaults to 1
+            floatIntensity={1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+            floatingRange={[0, 0.65]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+          >
+          <Chair />
+          
+          </Float>
+        </Stage>
+        <mesh rotation={[-Math.PI / 2, 0, 0]} position-y={-2}>
+          <planeGeometry args={[170, 170]} />
+          <MeshReflectorMaterial
+            blur={[300, 100]}
+            resolution={2048}
+            mixBlur={1}
+            mixStrength={40}
+            roughness={1}
+            depthScale={1.2}
+            minDepthThreshold={0.4}
+            maxDepthThreshold={1.4}
+            color="#101010"
+            metalness={0.5}
+          />
+        </mesh>
+      </PresentationControls>
+    </>
+  );
+};
+
+export default Experience;
